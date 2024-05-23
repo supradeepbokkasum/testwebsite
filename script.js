3645c8400cae46f237e896a7e0b93e5e1e49c22c
supradeepbokkasum <supradeep.bokkasum@gmail.com>
2024-05-23 21:29:44 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Added to cart!');
+        });
+    });
+});
